b71806b30f6f60755bb7e0b817989d171df9206a
spsstudent15 <armenoush@gmail.com>
2018-05-10 18:43:36 -0400

    Updated app files

--- app.R
+++ app.R
@@ -4,6 +4,7 @@
 library(shiny)
 library(plotly)
 
+#dependencies
 df <- read.csv("sal-title-year-comparison-long.csv")
 dft <- read.csv("titledescriptions.csv")
 dfa <- read.csv("sal-agency-year-comparison-long.csv")
@@ -26,7 +27,7 @@ br()),
       
 tabPanel("Job Titles", 
   fluidPage(
-  headerPanel('Hiring and Salaries by Title'),
+  headerPanel('Hiring and Salaries by Title - Most Common Titles Only'),
   sidebarPanel(
     p("Select a civil service title below to see the average salary and count of jobs by year. 
       This is a select list of titles represented by the most agencies."),
@@ -35,6 +36,33 @@ tabPanel("Job Titles",
     verbatimTextOutput('stats'),
     p("Description of the selected title:"),
     verbatimTextOutput('describe1'),
+
+    tags$h6("The following titles are represented. 
+            These are the 20 titles that are most common across all city agencies, 
+            listed here in order of most common to least common.
+            This select group of titles was chosen to normalize the data across agencies."),
+    
+    tags$h6("COMMUNITY ASSOCIATE,
+  ADMINISTRATIVE STAFF ANALYST,
+  COMMUNITY COORDINATOR,
+  PRINCIPAL ADMINISTRATIVE ASSOC,
+  CLERICAL ASSOCIATE,
+  COMPUTER SYSTEMS MANAGER,
+  EXECUTIVE AGENCY COUNSEL,
+  ADM MANAGER-NON-MGRL FRM M1/M2,
+  ASSOCIATE STAFF ANALYST,
+  COMMUNITY ASSISTANT,
+  SECRETARY,
+  ADMINISTRATIVE PUBLIC INFORMAT,
+  AGENCY ATTORNEY,
+  COMPUTER SPECIALIST (SOFTWARE),
+  STAFF ANALYST,
+  ADMINISTRATIVE MANAGER,
+  COMPUTER ASSOC (SOFTWARE),
+  PROCUREMENT ANALYST,
+  COMPUTER ASSOC (OPERATIONS),
+  ACCOUNTANT
+"),
     tags$h5("Source: NYC Open Data")
   ),
   mainPanel(
@@ -46,10 +74,39 @@ tabPanel("Job Titles",
 
 tabPanel("Agencies", 
          fluidPage(
-           headerPanel('Hiring and Salaries by Agency'),
+           headerPanel('Hiring and Salaries by Agency  - Most Common Titles Only'),
            sidebarPanel(
              p("Select an agency below to see the average salary and count of jobs by year."),
-             selectInput('agency', 'Agency Name', unique(dfa$agency), selected='ADMINISTRATION FOR CHILDRE'),
+             selectInput('agency', 'Agency Name', unique(dfa$agency), 
+                         selected='ADMINISTRATION FOR CHILDRE'),
+             tags$h6("The following titles are represented. 
+            These are the 20 titles that are most common across all city agencies, 
+            listed here in order of most common to least common.
+            This select group of titles was chosen to normalize the data across agencies."),
+             
+             tags$h6("COMMUNITY ASSOCIATE,
+  ADMINISTRATIVE STAFF ANALYST,
+  COMMUNITY COORDINATOR,
+  PRINCIPAL ADMINISTRATIVE ASSOC,
+  CLERICAL ASSOCIATE,
+  COMPUTER SYSTEMS MANAGER,
+  EXECUTIVE AGENCY COUNSEL,
+  ADM MANAGER-NON-MGRL FRM M1/M2,
+  ASSOCIATE STAFF ANALYST,
+  COMMUNITY ASSISTANT,
+  SECRETARY,
+  ADMINISTRATIVE PUBLIC INFORMAT,
+  AGENCY ATTORNEY,
+  COMPUTER SPECIALIST (SOFTWARE),
+  STAFF ANALYST,
+  ADMINISTRATIVE MANAGER,
+  COMPUTER ASSOC (SOFTWARE),
+  PROCUREMENT ANALYST,
+  COMPUTER ASSOC (OPERATIONS),
+  ACCOUNTANT
+"),
+             
+             
              tags$h5("Source: NYC Open Data")
              ),
            mainPanel(
